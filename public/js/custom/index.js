9eccd41c5c6de1944c2109915be7544e74a20724
Morris <morrisk0301@gmail.com>
2019-06-14 17:41:01 +0900

    update

--- public/js/custom/index.js
+++ public/js/custom/index.js
@@ -315,13 +315,12 @@ window.onload = async function () {
     const firstDay = new Date(today.getFullYear(), today.getMonth(), 1).toUTCString();
     const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0).toUTCString();
     const apMonthNum = await getAppointmentNum(firstDay, lastDay, true);
-    const date = daysInMonth(new Date().getMonth()+1, new Date().getFullYear());
 
     $("#user_num").text(memberNum);
     $("#appointment_num").text(apNum);
     $("#appointment_month_num").text(apMonthNum);
     $("#month").text(new Date().getMonth()+1);
-    $("#appointment_dayavg").text(Math.round(apMonthNum/date));
+    $("#appointment_dayavg").text(Math.round(apMonthNum/moment().date()));
 
 
 };
